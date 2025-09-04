a583bc8ce58ad060cadcb6782ebc4d0df8b0ad2a
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-09-04 14:09:09 -0400

    Create fav_movies.py

---
+++ Labs/lab01/fav_movies.py
@@ -0,0 +1,22 @@
+""" 
+    File : Optimisim_print.py
+    Author : Jayden Mautsa 
+    Date : 9/4/2025
+    Section : Homework 
+    E-mail : jmautsa1@umbc.edu
+    Description : This is an example file for learning 
+    """
+    
+#user input for movies 
+horror = str(input("What is your favorite horror/thriller movie? "))
+sci_fi = str(input("What is your favourate sci-fi movie? "))
+romantic = str(input("What is your favourate romantic movie? "))
+movie = str(input("What is your favourate movie of all time? "))
+
+#user input display output
+print(
+    f"Your favorite horror/thriller movie: {horror}\n"
+    f"Your favourite sci-fi movie: {sci_fi}\n"
+    f"Your favourite romantic movie is: {romantic}\n"
+    f"Of course the best movie of all time is: {movie}"
+)
