3a5730f8609d03afa768fe5336823a1f3b93db96
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-09-04 14:09:07 -0400

    Create Worst_caculator-Application setup.py

---
+++ Labs/lab01/Worst_caculator-Application setup.py
@@ -0,0 +1,32 @@
+
+""" 
+    File : Optimisim_print.py
+    Author : Jayden Mautsa 
+    Date : 9/4/2025
+    Section : Homework 
+    E-mail : jmautsa1@umbc.edu
+    Description : This is an example file for learning 
+    """
+
+#user input ints
+num_int =  int(input("Enter integer 1: "))
+num_int2 = int(input("Enter integer 2: "))
+
+#user input floats 
+num_float =  float(input("Enter float 1: "))
+num_float2 = float(input("Enter float 2: "))
+num_float3 = float(input("Enter float 3: "))
+
+#sum of the 2 intgers 
+sum = num_int + num_int2
+
+#product of the 3 floats 
+sum2 = num_float * num_float2 * num_float3
+
+#display of the prompts 
+print(
+    f"The sum of the integers {num_int} and {num_int2} is {sum}"
+    f"\nThe product the floats {num_float}, {num_float2} and {num_float3} is  {sum2}" 
+)
+
+
