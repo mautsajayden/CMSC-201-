8262f9cb0d119d7f743a69b708baecc337327954
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-09-12 23:50:18 -0400

    Create MadPyLib.py

---
+++ Homework/Homework1/MadPyLib.py
@@ -0,0 +1,25 @@
+"""
+    File : MadPyLib
+    Author : Jayden Mautsa 
+    Date : 9/12/2025
+    Section : Homework 
+    E-mail : jmautsa1@umbc.edu
+    Description : This is about a story madpylib game 
+    """
+
+#user input 
+name = input("Ask the user their name. ") 
+
+subject = input("Ask the user for a subject/noun. ")
+
+adjective = input("Ask for an adjective. ")
+
+verb = input("Ask for a verb. ")
+
+noun = input("Ask for another noun. ")
+
+#output of the users input
+print(
+    f"Hello {name}, we are going to have an amazing semester learning {subject}, it's " 
+    f"going to be {adjective} so don't worry if you need to {verb} from a {noun}.")
+
