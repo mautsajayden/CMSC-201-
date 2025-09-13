71a32916c459f5dcf740c3c28dc8341ec4aae0e1
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-09-12 23:50:15 -0400

    Create Favorite Things.py

---
+++ Homework/Homework1/Favorite Things.py
@@ -0,0 +1,14 @@
+""" 
+    File : Favorite Things
+    Author : Jayden Mautsa 
+    Date : 9/12/2025
+    Section : Homework 
+    E-mail : jmautsa1@umbc.edu
+    Description : This is about my favourate food. 
+    """
+
+print(
+    "My favorite food is sushi." 
+    "My favorite movie is The Fifth Element."
+    "My favorite music is mongolian throat singing."
+    )
