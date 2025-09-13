186fd6315bbcdfec6d326292d2462116c97a5855
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-09-12 23:50:24 -0400

    Create Teaspoons.py

---
+++ Homework/Homework1/Teaspoons.py
@@ -0,0 +1,11 @@
+""" 
+    File : Teaspoons
+    Author : Jayden Mautsa 
+    Date : 9/12/2025
+    Section : Homework 
+    E-mail : jmautsa1@umbc.edu
+    Description : This is about cornverting teaspoons to liquid 
+    """
+
+teaToLiquid = float(input("Input a number of teaspoons: "))
+print(f"The number of milliliters is {teaToLiquid*(4.929)}")
