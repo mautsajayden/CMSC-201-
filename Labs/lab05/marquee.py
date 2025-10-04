22bc5b2b0696f9735be60e4924a4d25a746b6588
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-10-03 21:47:45 -0400

    Create marquee.py

---
+++ Labs/lab05/marquee.py
@@ -0,0 +1,42 @@
+import time
+
+
+
+word = input("What text do you want to scroll? ")
+
+num = len(word)
+for i in range(num):
+    time.sleep(0.25)
+    print(word[i:] + word[:i])
+
+
+#part 1
+
+text = input("Enter a string: ")
+
+text = text.strip()
+
+parts = text.split()
+
+joined = "".join(parts)
+
+result = joined.lower()
+
+print(result)
+
+
+#part 2 
+
+text = input("Enter a string: ")
+
+
+text = text.strip()
+
+parts = text.split()
+
+word = "".join(parts)
+
+word = word.lower()
+
+for i in range(len(word)):
+    print(i, word[i])
