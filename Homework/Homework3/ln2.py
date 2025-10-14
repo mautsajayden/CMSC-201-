5271eb8459c1282177e0f19e736a8424c99c48a3
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-10-14 15:21:20 -0400

    Update ln2.py

--- Homework/Homework3/ln2.py
+++ Homework/Homework3/ln2.py
@@ -12,7 +12,7 @@ n = int(input("How many terms do you want to calculate? "))
 total = 0
 
 for i in range(1, n + 1):
-    sign = (-1) ** (i + 1)   # alternates between +1 and -1
+    sign = (-1) ** (i + 1)   
     total += sign * (1 / i)
 
 print(f"The approximation of ln(2) for {n} terms is {total}")
