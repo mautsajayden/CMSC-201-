91d62bc34fb65079ff569778a6d2cac8515e1c60
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-10-31 22:25:36 -0400

    Update lab9.py

--- Labs/lab09/lab9.py
+++ Labs/lab09/lab9.py
@@ -1,11 +1,10 @@
 """
-File:    FILENAME.py
-Author:  YOUR NAME
-Date:    THE DATE
-Section: YOUR DISCUSSION SECTION NUMBER
-E-mail:  YOUR_EMAIL@umbc.edu
-Description:
-  DESCRIPTION OF WHAT THE PROGRAM DOES
+File:    lab.py
+Author:  Jayden Mautsa
+Date:    10/30/2025
+Section: 36
+E-mail:  jmautsa1@umbc.edu
+Description: its creating a 2d list 
 """
 
 
@@ -38,7 +37,18 @@ def create_new_not_weird_2d_list(height, width, value):
     :return: a 2d list
     """
     # hint hint hint: make two nested for loops that utilize range()
-    pass
+    
+    mat = []
+    
+    for i in range(height):          
+        row = []
+        
+        for j in range(width):       
+            row.append(value)  
+                  
+        mat.append(row)              
+        
+    return mat
 
 
 if __name__ == '__main__':
@@ -47,4 +57,10 @@ if __name__ == '__main__':
     matrix[2][3] = 2
     print(matrix)
     # I'm expecting [[0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2], [0, 0, 0, 0]]
-    # but what do I get...?
+    
+    # but what do I get..[0, 1, 0, 2], [0, 1, 0, 2], [0, 1, 0, 2], [0, 1, 0, 2].?
+
+    matrix = create_new_not_weird_2d_list(4, 4, 0)
+    matrix[0][1] = 1
+    matrix[2][3] = 2
+    print(matrix)
